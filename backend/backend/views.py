43e00638fe60e7f8f9d4ce8eb07295f2766e6f0a
Marco Ceppi <marco@ceppi.net>
2014-08-15 12:35:43 -0400

    Provide all records, not just the query

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -19,5 +19,5 @@ from .models import (
 def dashboard(request):
     #reviews = DBSession.query(Review).group_by(Review.review_category_id).all()
     reviews = DBSession.query(Review).filter(Review.state != 'REVIEWED', Review.state != 'NEW', Review.state != 'IN PROGRESS').order_by(Review.updated).all()
-    incoming = DBSession.query(Review).filter_by(state='NEW').order_by(Review.updated)
+    incoming = DBSession.query(Review).filter_by(state='NEW').order_by(Review.updated).all()
     return dict(reviews=reviews, incoming=incoming)
