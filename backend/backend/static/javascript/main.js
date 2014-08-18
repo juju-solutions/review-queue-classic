9f7c5ad9f2785a205dd145251e9f8b1d899b53c2
Marco Ceppi <marco@ceppi.net>
2014-08-18 15:24:45 -0400

    Filtering for long lists

--- backend/backend/static/javascript/main.js
+++ backend/backend/static/javascript/main.js
@@ -1,3 +1,10 @@
 $(function() {
   $('table').tablesort();
+  $('.ui.checkbox').checkbox();
+  $('.ui.checkbox input').change(function() {
+    $(this).parents().eq(3).find('.' + $(this).attr('name')).toggle();
+  });
+  $('h1 .icon.filter').click(function() {
+    $(this).closest('.review').children('.filter.controls').slideToggle();
+  });
 });
