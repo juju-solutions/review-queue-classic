f653fac1b6c357fcca9b7301cb18f8daebcd7885
Marco Ceppi <marco@ceppi.net>
2014-09-04 12:11:31 -0400

    Format to semantic UI standards

--- backend/backend/static/javascript/main.js
+++ backend/backend/static/javascript/main.js
@@ -12,11 +12,16 @@ var states = {
 $(function() {
   $('table').tablesort();
   $('.ui.checkbox').checkbox();
-  $('.ui.checkbox input').change(function() {
-    $(this).parents().eq(3).find('.' + $(this).attr('name')).toggle();
-  });
-  $('h1 .icon.filter').click(function() {
-    $(this).closest('.review').children('.filter.controls').slideToggle();
+  $('.ui.checkbox input')
+    .change(function() {
+      $(this).parents().eq(3).find('.' + $(this).attr('name')).toggle();
+    })
+  ;
+  $('h1 .icon.filter')
+    .click(function() {
+      $(this).closest('.review').children('.filter.controls').slideToggle();
+    })
+  ;
   $('.state').each(function() {
     $(this).data('content', states[$(this).data('sort').toLowerCase()]);
     $(this).data('title', $(this).text());
