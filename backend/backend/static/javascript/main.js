4d7fe7cbd69de455002ccb995fb00012269d679b
Marco Ceppi <marco@ceppi.net>
2014-09-04 12:11:20 -0400

    Display states

--- backend/backend/static/javascript/main.js
+++ backend/backend/static/javascript/main.js
@@ -1,3 +1,14 @@
+var states = {
+  'pending': 'This item pending a review. If you are the owner, sit tight and someone will review shortly!',
+  'followup': 'The author has replied since the review of this item.',
+  'ready': 'Your merge has been approved and will land in the charm store soon!',
+  'abandonded': 'This has been marked as abandonded and is no longer considered a valid review',
+  'reviewed': 'Feedback by the charm community has been placed and follow up by the author is required',
+  'inprogress': 'Review has been passed and the item needs more work by the author',
+  'closed': 'This review has been completed and the charm in the store!',
+  'merged': 'This fix has landed and is in the charm store!'
+};
+
 $(function() {
   $('table').tablesort();
   $('.ui.checkbox').checkbox();
@@ -6,5 +17,11 @@ $(function() {
   });
   $('h1 .icon.filter').click(function() {
     $(this).closest('.review').children('.filter.controls').slideToggle();
+  $('.state').each(function() {
+    $(this).data('content', states[$(this).data('sort').toLowerCase()]);
+    $(this).data('title', $(this).text());
   });
+
+  $('.state').popup();
+  $('[data-content]').popup();
 });
