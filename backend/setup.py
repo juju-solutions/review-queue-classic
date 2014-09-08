76763812ac1ea8468906864a17bf608191346200
Marco Ceppi <marco@ceppi.net>
2014-09-08 14:36:22 -0400

    version bump

--- backend/setup.py
+++ backend/setup.py
@@ -26,7 +26,7 @@ requires = [
     ]
 
 setup(name='backend',
-      version='1.2.0',
+      version='1.3.0',
       description='backend',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
